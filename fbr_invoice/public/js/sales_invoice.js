b601bd87318db073594ae5ef85716848916148c2
Mohammad Ali <swe.mirza.ali@gmail.com>
2021-09-14 17:13:12 +0500

    fbr changes, now sending request from js side

--- fbr_invoice/public/js/sales_invoice.js
+++ fbr_invoice/public/js/sales_invoice.js
@@ -55,7 +55,7 @@ frappe.ui.form.on("Sales Invoice", {
 
             $.ajax('http://127.0.0.1:8524/api/IMSFiscal/GetInvoiceNumberByModel', {
                 type: 'POST',  // http method
-                data: JSON.stringify(data),  // data to submit
+                data: data,  // data to submit
                 success: function (data, status, xhr) {
                     debugger;
                     console.log('status: ' + status + ', data: ' + JSON.stringify(data));
